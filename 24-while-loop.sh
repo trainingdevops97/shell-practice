1ca8d30696ed1f53d23115e1a5d48164bd289e7a
Sivakumar Reddy M <info@joindevops.com>
2026-02-03 08:45:13 +0530

    actions

--- 24-while-loop.sh
+++ 24-while-loop.sh
@@ -2,10 +2,15 @@
 
 count=1
 
-while [ $count -le 6 ]
-do
-  echo "Count is $count"
-  sleep 1
-  # Increment the counter
-  ((count++)) 
-done
+# while [ $count -le 5 ]
+# do
+#   echo "Count is $count"
+#   sleep 1
+#   # Increment the counter
+#   ((count++)) 
+# done
+
+while IFS= read -r line; do
+  # Process each line here
+  echo "$line"
+done < ./21-script.sh # input which file to read
