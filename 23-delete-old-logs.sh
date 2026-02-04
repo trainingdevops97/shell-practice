25af056cc0549d9502ce22e527bb07b27217f4e6
Sivakumar Reddy M <info@joindevops.com>
2026-02-04 14:51:34 +0530

    actions

--- 23-delete-old-logs.sh
+++ 23-delete-old-logs.sh
@@ -6,7 +6,7 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_DIR=/home/ec2-user/app-logs
-LOGS_FILE="$LOGS_DIR/$0.log"
+LOGS_FILE="$LOGS_DIR/$(basename "$0").log"
 
 if [ ! -d $LOGS_DIR ]; then
     echo -e "$LOGS_DIR does not exitst"
