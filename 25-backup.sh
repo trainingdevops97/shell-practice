99aa72355def66062283d1eabf049efa4081b495
Sivakumar Reddy M <info@joindevops.com>
2026-02-04 08:02:49 +0530

    actions

--- 25-backup.sh
+++ 25-backup.sh
@@ -48,7 +48,7 @@ log "Source Directory: $SOURCE_DIR"
 log "Destination Directory: $DEST_DIR"
 log "Days: $DAYS"
 
-if [ -z $FILES ]; then
+if [ -z ${FILES} ]; then
     log "No files to archieve ... $Y Skipping $N"
 else
     # app-logs-$timestamp.zip
