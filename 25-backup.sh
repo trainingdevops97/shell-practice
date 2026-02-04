46e3d6b935c3d1641090c8b796f4e9b8379b648a
Sivakumar Reddy M <info@joindevops.com>
2026-02-04 07:45:38 +0530

    actions

--- 25-backup.sh
+++ 25-backup.sh
@@ -7,7 +7,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # 14 days is the default value, if the user not supplied
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N"
@@ -22,4 +24,14 @@ USAGE(){
 
 if [ $# -lt 2 ]; then
     USAGE
+fi
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo -e "$R $SOURCE_DIR does not exist $N"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+    echo -e "$R $DEST_DIR does not exist $N"
+    exit 1
 fi
