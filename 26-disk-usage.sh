567b06db8894050199b1d1355cafbc9e54b4ab7f
Sivakumar Reddy M <info@joindevops.com>
2026-02-04 08:48:58 +0530

    actions

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -19,8 +19,8 @@ do
     PARTITION=$(echo $line | awk '{print $7}')
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE+="High Disk usage on $PARTITION:$USAGE"
+        MESSAGE+="High Disk usage on $PARTITION:$USAGE% \n"
     fi
 done <<< $DISK_USAGE
 
-echo "$MESSAGE"
+echo -e "$MESSAGE"
